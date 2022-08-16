35c6a9384e3ab9853015a6088e48a31c8eb7c676
Erik <81779992+UpperErik@users.noreply.github.com>
2022-08-16 15:41:17 -0500

    Add files via upload

--- Count_of positives_sum_of_negatives.js
+++ Count_of positives_sum_of_negatives.js
@@ -2,7 +2,7 @@ function countPositivesSumNegatives(input) {
     // your code here
     let positiveN = 0;
     let negativeN = 0;
-    if (input.length === 0 || input == NaN) {
+    if (input.length === 0 || input === null) {
         return [""];
     } else {
         input.forEach((numX) => numX > 0 ? positiveN++ : negativeN += numX)
